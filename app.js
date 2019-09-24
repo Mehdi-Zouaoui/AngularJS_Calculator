7dd750c3a036156a92a886490efcb57d4ff4725b
Mehdi <md1.zouaoui@gmail.com>
2019-09-24 12:35:26 +0200

    Operation Display added just above digit display. Now you can fully see what you're calculating

--- app.js
+++ app.js
@@ -8,23 +8,21 @@ angular.module("main").controller("mainController", function ($scope) {
     ctrl.data = "Calculatrice";
     ctrl.entier = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     ctrl.calc = [];
+    ctrl.index = 0;
     ctrl.affichage = null;
     ctrl.digitCtrl = "";
-    ctrl.operation = 0;
+    ctrl.showOperation = "";
     ctrl.result = 0;
-    ctrl.operators = ["+", "-", "/", "*"];
+    ctrl.operators = ["+", "-", "/", "*","="];
     ctrl.currentOperator = "";
     ctrl.operationDisplay = [];
     ctrl.operatorsDisplay = [];
 
-   ctrl.calculatorDisplay = function(){
+    ctrl.calculatorDisplay = function () {
         if (ctrl.digitCtrl === "") {
             ctrl.affichage = ctrl.result;
         } else ctrl.affichage = ctrl.digitCtrl;
         return ctrl.affichage;
-    };
-
-    ctrl.showResult = function (tab) {
     };
 
     ctrl.clear = function () {
@@ -34,19 +32,21 @@ angular.module("main").controller("mainController", function ($scope) {
         ctrl.isClear = true;
     };
 
-    ctrl.onOperatorClic = function(input) {
+    ctrl.onOperatorClic = function (input) {
         ctrl.isClear = false;
         ctrl.currentOperator = input;
         ctrl.operatorsDisplay.push(ctrl.currentOperator);
+        ctrl.operationDisplay.push(ctrl.digitCtrl);
         if (ctrl.calc.length < 2) {
-            if(ctrl.calc.length === 0 ){
+            if (ctrl.calc.length === 0) {
                 ctrl.result = ctrl.digitCtrl;
             }
             ctrl.calc.push(ctrl.digitCtrl);
             ctrl.digitCtrl = "";
-        } if (ctrl.calc.length === 2) {
-            for(let i = 0 ; i < ctrl.operatorsDisplay.length ; i++) {
-                switch (ctrl.operatorsDisplay[i-1]) {
+        }
+        if (ctrl.calc.length === 2) {
+            for (let i = 0; i < ctrl.operatorsDisplay.length; i++) {
+                switch (ctrl.operatorsDisplay[i - 1]) {
                     case "+" :
                         ctrl.result = Number(ctrl.calc[0]) + Number(ctrl.calc[1]);
                         break;
@@ -59,21 +59,26 @@ angular.module("main").controller("mainController", function ($scope) {
                     case "/" :
                         ctrl.result = Number(ctrl.calc[0]) / Number(ctrl.calc[1]);
                         break;
+                    case "=":
+                        ctrl.result = Number(ctrl.calc[1]);
                 }
             }
             ctrl.calc = [];
             ctrl.calc.push(ctrl.result);
-
-            ctrl.operationDisplay.push(ctrl.result);
-
-
         }
     };
 
-    ctrl.onDigitClic = function(input){
-        ctrl.digitCtrl += input ;
+    ctrl.onDigitClic = function (input) {
+        ctrl.digitCtrl += input;
     };
-    ctrl.fullCalcul = function(){
 
-    }
- });
+    ctrl.showCalcul = function (operator) {
+        ctrl.showOperation += ctrl.operationDisplay[ctrl.index] +' '+  ctrl.operatorsDisplay[ctrl.index] +' ';
+        console.log(ctrl.showOperation);
+        ctrl.index += 1;
+        if(operator ==="="){
+            ctrl.showOperation += ctrl.result
+        }
+    };
+
+});
