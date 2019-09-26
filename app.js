3263be4cbc3d37a46475f1ac08786b84b399c6f4
Mehdi <md1.zouaoui@gmail.com>
2019-09-26 11:04:35 +0200

    multiple operator plus function added

--- app.js
+++ app.js
@@ -4,19 +4,21 @@ angular.module("main", []);
 
 angular.module("main").controller("mainController", function ($scope) {
     const ctrl = this;
-    ctrl.numberInput = 0;
     ctrl.data = "Calculatrice";
-    ctrl.entier = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    ctrl.entier = [".", '±', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     ctrl.calc = [];
     ctrl.index = 0;
     ctrl.affichage = null;
     ctrl.digitCtrl = "";
     ctrl.showOperation = "";
     ctrl.result = 0;
-    ctrl.operators = ["+", "-", "/", "*","="];
+    ctrl.operators = ["+", "-", "/", "*", "=", "x²", "x³", "√x", "1/x"];
     ctrl.currentOperator = "";
     ctrl.operationDisplay = [];
     ctrl.operatorsDisplay = [];
+    ctrl.tampon = false;
+    ctrl.tamponDisplay = [];
+    ctrl.tamponIndex = 0;
 
     ctrl.calculatorDisplay = function () {
         if (ctrl.digitCtrl === "") {
@@ -26,17 +28,28 @@ angular.module("main").controller("mainController", function ($scope) {
     };
 
     ctrl.clear = function () {
+        ctrl.tampon = true;
+        ctrl.tamponDisplay.push(ctrl.showOperation);
+        ctrl.tamponIndex += 1;
+        ctrl.showOperation = "";
         ctrl.result = 0;
+        ctrl.operationDisplay = [];
+        ctrl.operatorsDisplay = [];
+        ctrl.index = 0;
         ctrl.calc = [];
         ctrl.digitCtrl = "";
         ctrl.isClear = true;
     };
 
     ctrl.onOperatorClic = function (input) {
+        if(ctrl.digitCtrl ===""){
+            ctrl.digitCtrl = 0
+        }
         ctrl.isClear = false;
         ctrl.currentOperator = input;
-        ctrl.operatorsDisplay.push(ctrl.currentOperator);
         ctrl.operationDisplay.push(ctrl.digitCtrl);
+        ctrl.operatorsDisplay.push(ctrl.currentOperator);
+
         if (ctrl.calc.length < 2) {
             if (ctrl.calc.length === 0) {
                 ctrl.result = ctrl.digitCtrl;
@@ -48,35 +61,56 @@ angular.module("main").controller("mainController", function ($scope) {
             for (let i = 0; i < ctrl.operatorsDisplay.length; i++) {
                 switch (ctrl.operatorsDisplay[i - 1]) {
                     case "+" :
-                        ctrl.result = Number(ctrl.calc[0]) + Number(ctrl.calc[1]);
+                        ctrl.result = parseFloat(ctrl.calc[0]) + parseFloat(ctrl.calc[1]);
+                        console.log('test' + parseFloat(ctrl.calc[0]), parseFloat(ctrl.calc[1]));
                         break;
                     case "-" :
-                        ctrl.result = Number(ctrl.calc[0]) - Number(ctrl.calc[1]);
+                        ctrl.result = parseFloat(ctrl.calc[0]) - parseFloat(ctrl.calc[1]);
                         break;
                     case "*" :
-                        ctrl.result = Number(ctrl.calc[0]) * Number(ctrl.calc[1]);
+                        ctrl.result = parseFloat(ctrl.calc[0]) * parseFloat(ctrl.calc[1]);
                         break;
                     case "/" :
-                        ctrl.result = Number(ctrl.calc[0]) / Number(ctrl.calc[1]);
+                        ctrl.result = parseFloat(ctrl.calc[0]) / parseFloat(ctrl.calc[1]);
+                        break;
+                    case "x²" :
+                        ctrl.result = parseFloat(ctrl.calc[0]) * parseFloat(ctrl.calc[0]);
+                        break;
+                    case "x³" :
+                        ctrl.result = parseFloat(ctrl.calc[0]) * parseFloat(ctrl.calc[0]) * parseFloat(ctrl.calc[0]);
+                        break;
+                    case "√x":
+                        ctrl.result = Math.sqrt(parseFloat(ctrl.calc[0]));
+                        break;
+                    case "1/x":
+                        ctrl.result = 1 / parseFloat(ctrl.calc[0]);
                         break;
                     case "=":
-                        ctrl.result = Number(ctrl.calc[1]);
+                        ctrl.result = parseFloat(ctrl.calc[1]);
+                        ctrl.operationDisplay.push(ctrl.affichage);
+                        ctrl.tamponAffichage();
+
+                        break;
                 }
             }
             ctrl.calc = [];
             ctrl.calc.push(ctrl.result);
+
+        }
+         ctrl.showCalcul(input);
+    };
+    ctrl.onDigitClic = function (input) {
+        ctrl.numberInput = ctrl.val;
+        ctrl.digitCtrl += input;
+        if (input === '±') {
+            ctrl.digitCtrl = '-' + ctrl.affichage;
         }
     };
 
-    ctrl.onDigitClic = function (input) {
-        ctrl.digitCtrl += input;
-    };
-
     ctrl.showCalcul = function (operator) {
-        ctrl.showOperation += ctrl.operationDisplay[ctrl.index] +' '+  ctrl.operatorsDisplay[ctrl.index] +' ';
-        console.log(ctrl.showOperation);
+        ctrl.showOperation += ctrl.operationDisplay[ctrl.index] + ' ' + ctrl.operatorsDisplay[ctrl.index] + ' ';
         ctrl.index += 1;
-        if(operator ==="="){
+        if (operator === "=") {
             ctrl.showOperation += ctrl.result
         }
     };
